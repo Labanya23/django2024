ff05457d898e03bee15ae6c6cbcb73331720ea11
Labanya23 <100686917+Labanya23@users.noreply.github.com>
2024-11-25 00:43:15 +0600

    LEARNING DJANGO

---
+++ HRT/heart/heart/views.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+#from stores.models import Store
+
+
+def home(request):
+
+    return render(request, 'home.html', {
+        
+    })
+
+def nav1(request):
+
+    return render(request, 'nav1.html',{
+
+    })
+
+
+def about(request):
+
+    return render(request, 'about.html',{
+
+    })
